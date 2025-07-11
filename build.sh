3c650ddd635d7f47380bbcff03120cbbc38dda0d
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-07-11 15:21:59 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+# 先执行 prepare-packages.sh 此脚本用于拷贝所有自定义ipk到packages目录
+sh prepare-packages.sh
+# 以下是仓库内的包名 你可以在openwrt官网仓库查询插件名称
+# https://downloads.openwrt.org/releases/24.10.2/packages/x86_64/luci/
+# https://mirrors.aliyun.com/openwrt/releases/24.10.2/packages/x86_64/luci/
+BASE_PACKAGES=""
+BASE_PACKAGES="$BASE_PACKAGES curl"
+BASE_PACKAGES="$BASE_PACKAGES -dnsmasq"
+# 此处为什么勾选完整版dnsmasq-full 因为openclash需要该依赖 由于dnsmasq和dnsmasq-full 在安装上互斥 因此只能保留一个 减号代表去除
+BASE_PACKAGES="$BASE_PACKAGES dnsmasq-full"
+BASE_PACKAGES="$BASE_PACKAGES luci"
+BASE_PACKAGES="$BASE_PACKAGES bash"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-ttyd-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES openssh-sftp-server"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-package-manager-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-compat"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-firewall-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-base-zh-cn"
+# 如果勾选了docker 务必保证软件包大小ROOTFS_PARTSIZE至少大于512 目前1024
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-dockerman-zh-cn"
+# 文件管理器
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-filemanager-zh-cn"
+# openlist用来平替alist 但阿里镜像仓库暂时没有 若勾选此项需在repositories.conf中启用官方源
+# 其实可以弯道实现安装openlist 因为istore商店里有该插件 你可以勾选下列的luci-app-store
+#BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openlist-zh-cn"
+
+#打印机相关
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-p910nd-zh-cn"
+#BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openvpn-zh-cn"
+
+
+
+# 下面是自定义的包 你可以用#注释掉不需要的包 也可以添加更多的包 
+# 使用条件:在extra-packages下放置了相关run或者ipk
+CUSTOM_PACKAGES=""
+# 第三方插件 文件传输 luci-app-filetransfer
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lib-fs"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lua-runtime"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-filetransfer"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-filetransfer-zh-cn"
+# 第三方插件 argon主题 luci-theme-argon 紫色主题 3个ipk
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-argon-config-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-argon-config"
+
+# 第三方插件 istore 应用商店
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-store"
+# 第三方插件 首页和网络向导
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-quickstart"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+
+# 第三方插件 luci-app-adguardhome 去广告
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+# 第三方插件 openclash 内核放在files/etc/openclash/core/clash_meta 若不勾选则不集成
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
+# 第三方插件 luci-app-passwall 包含内部组件
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-passwall-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES xray-core"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES sing-box"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES hysteria"
+# 第三方插件 luci-app-ssr-plus 尤其注意要包含 shadowsocks-libev-ss-server
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ssr-plus"
+# 第三方插件 luci-app-homeproxy
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-homeproxy-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-homeproxy"
+# 第三方插件 luci-app-nikki
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
+
+# ✅ 校验 CUSTOM_PACKAGES 中的包是否都存在于 packages_names.txt
+package_file="packages_names.txt"
+for pkg in $CUSTOM_PACKAGES; do
+  if ! grep -qx "$pkg" "$package_file"; then
+    echo "❌ 错误：包 $pkg 不存在于 $package_file 中 请检查自定义包名是否正确!"
+    exit 1
+  fi
+done
+
+# 拼接
+PACKAGES="$BASE_PACKAGES $CUSTOM_PACKAGES"
+
+# 若构建openclash 则添加内核
+if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
+    echo "✅ [构建逻辑] 已选择 luci-app-openclash，添加 openclash core"
+    mkdir -p files/etc/openclash/core
+    if [ -f extra-packages/temp-unpack/clash_meta ]; then
+        cp extra-packages/temp-unpack/clash_meta files/etc/openclash/core/clash_meta
+    else
+        echo "⚠️ [警告] 缺少 clash_meta 内核,跳过复制,你应该确保extra-packages目录下有相关run文件"
+    fi
+else
+    echo "⚪️ [构建逻辑] 未选择 luci-app-openclash"
+    [ -d files/etc/openclash ] && rm -rf files/etc/openclash
+fi
+
+
+# 若构建luci-app-adguardhome 则添加内核
+if echo "$PACKAGES" | grep -q "luci-app-adguardhome"; then
+    echo "✅ [构建逻辑] 已选择 luci-app-adguardhome，添加 AdGuardHome core"
+    if [ -f extra-packages/temp-unpack/AdGuardHome/AdGuardHome ]; then
+        cp extra-packages/temp-unpack/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome
+    else
+        echo "⚠️ [警告] 缺少 AdGuardHome内核,跳过复制,你应该确保extra-packages目录下有相关run文件"
+    fi
+else
+    echo "⚪️ [构建逻辑] 未选择 luci-app-adguardhome"
+    [ -f files/usr/bin/AdGuardHome ] && rm -f files/usr/bin/AdGuardHome
+fi
+
+
+# 开始构建 软件包大小1024代表1GB 
+# 可选参数FILES=files 代表files目录中若有文件 则覆盖openwrt的根目录 原样注入  
+# 例如files/etc对应覆盖openwrt系统/etc目录中的文件 
+# 例如files/mnt对应覆盖openwrt系统/mnt目录中的文件 
+OUTPUT_DIR=$(pwd)/output
+mkdir -p $OUTPUT_DIR
+make image PROFILE=generic PACKAGES="$PACKAGES"  FILES=files ROOTFS_PARTSIZE=1024 BIN_DIR=$OUTPUT_DIR
+ls -lah $OUTPUT_DIR
---
+++ files/etc/uci-defaults/99-init.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+
+# 设置默认wan口防火墙打开，方便虚拟机用户首次访问 webui
+uci set firewall.@zone[1].input='ACCEPT'
+uci commit firewall
+
+# 检查 dockerd 是否已安装 设置防火墙规则 让docker的子网扩大范围 '172.16.0.0/12'
+if command -v dockerd >/dev/null 2>&1; then
+    echo "检测到 Docker，正在配置防火墙规则..."
+    FW_FILE="/etc/config/firewall"
+
+    # 删除所有名为 docker 的 zone
+    uci delete firewall.docker
+
+    # 先获取所有 forwarding 索引，倒序排列删除
+    for idx in $(uci show firewall | grep "=forwarding" | cut -d[ -f2 | cut -d] -f1 | sort -rn); do
+        src=$(uci get firewall.@forwarding[$idx].src 2>/dev/null)
+        dest=$(uci get firewall.@forwarding[$idx].dest 2>/dev/null)
+        echo "Checking forwarding index $idx: src=$src dest=$dest"
+        if [ "$src" = "docker" ] || [ "$dest" = "docker" ]; then
+            echo "Deleting forwarding @forwarding[$idx]"
+            uci delete firewall.@forwarding[$idx]
+        fi
+    done
+    # 提交删除
+    uci commit firewall
+    # 追加新的 zone + forwarding 配置
+    cat <<EOF >>"$FW_FILE"
+
+config zone 'docker'
+  option input 'ACCEPT'
+  option output 'ACCEPT'
+  option forward 'ACCEPT'
+  option name 'docker'
+  list subnet '172.16.0.0/12'
+
+config forwarding
+  option src 'docker'
+  option dest 'lan'
+
+config forwarding
+  option src 'docker'
+  option dest 'wan'
+
+config forwarding
+  option src 'lan'
+  option dest 'docker'
+EOF
+
+else
+    echo "未检测到 Docker，跳过防火墙配置。"
+fi
+
+# 设置主机名映射，解决安卓原生TV首次连不上网的问题
+uci add dhcp domain
+uci set "dhcp.@domain[-1].name=time.android.com"
+uci set "dhcp.@domain[-1].ip=203.107.6.88"
+uci commit dhcp
+
+# 计算网卡数量
+count=0
+ifnames=""
+for iface in /sys/class/net/*; do
+    iface_name=$(basename "$iface")
+    # 检查是否为物理网卡（排除回环设备和无线设备）
+    if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
+        count=$((count + 1))
+        ifnames="$ifnames $iface_name"
+    fi
+done
+# 删除多余空格
+ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
+# 网络设置
+if [ "$count" -eq 1 ]; then
+    # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
+    # 单网口设备 不支持修改ip 不要在此处修改ip
+    uci set network.lan.proto='dhcp'
+elif [ "$count" -gt 1 ]; then
+    # 提取第一个接口作为WAN
+    wan_ifname=$(echo "$ifnames" | awk '{print $1}')
+    # 剩余接口保留给LAN
+    lan_ifnames=$(echo "$ifnames" | cut -d ' ' -f2-)
+    # 设置WAN接口基础配置
+    uci set network.wan=interface
+    # 提取第一个接口作为WAN
+    uci set network.wan.device="$wan_ifname"
+    # WAN接口默认DHCP
+    uci set network.wan.proto='dhcp'
+    # 设置WAN6绑定网口eth0
+    uci set network.wan6=interface
+    uci set network.wan6.device="$wan_ifname"
+    # 更新LAN接口成员
+    # 查找对应设备的section名称
+    section=$(uci show network | awk -F '[.=]' '/\.@?device\[\d+\]\.name=.br-lan.$/ {print $2; exit}')
+    if [ -z "$section" ]; then
+        echo "error：cannot find device 'br-lan'." >>$LOGFILE
+    else
+        # 删除原来的ports列表
+        uci -q delete "network.$section.ports"
+        # 添加新的ports列表
+        for port in $lan_ifnames; do
+            uci add_list "network.$section.ports"="$port"
+        done
+        echo "ports of device 'br-lan' are update." >>$LOGFILE
+    fi
+    # LAN口设置静态IP
+    uci set network.lan.proto='static'
+    # 多网口设备 支持修改为别的ip地址,别的地址应该是网关地址，形如192.168.xx.1 项目说明里都强调过。
+    # 大家不能胡乱修改哦 比如有人修改为192.168.99.55 这是错误的理解 这个项目不能提前设置旁路地址
+    # 旁路的设置分2类情况,情况一是单网口的设备,默认是DHCP模式，ip应该在上一级路由器里查看。之后进入web页在设置旁路。
+    # 情况二旁路由如果是多网口设备，也应当用网关访问网页后，在自行在web网页里设置。总之大家不能直接在代码里修改旁路网关。千万不要徒增bug啦。
+    uci set network.lan.ipaddr='192.168.99.1'
+    uci set network.lan.netmask='255.255.255.0'
+fi
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+uci commit
+
+exit 0

39ee9b87f299ab516712e0cd45534cb5d24c5f7e
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2025-07-14 13:22:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-# 先执行 prepare-packages.sh 此脚本用于拷贝所有自定义ipk到packages目录
-#sh prepare-packages.sh
-# 以下是仓库内的包名 你可以在openwrt官网仓库查询插件名称
-# https://downloads.openwrt.org/releases/24.10.2/packages/x86_64/luci/
-# https://mirrors.aliyun.com/openwrt/releases/24.10.2/packages/x86_64/luci/
 BASE_PACKAGES=""
 BASE_PACKAGES="$BASE_PACKAGES curl"
 BASE_PACKAGES="$BASE_PACKAGES -dnsmasq"
-# 此处为什么勾选完整版dnsmasq-full 因为openclash需要该依赖 由于dnsmasq和dnsmasq-full 在安装上互斥 因此只能保留一个 减号代表去除
 BASE_PACKAGES="$BASE_PACKAGES dnsmasq-full"
 BASE_PACKAGES="$BASE_PACKAGES luci"
 BASE_PACKAGES="$BASE_PACKAGES bash"
@@ -18,17 +12,21 @@ BASE_PACKAGES="$BASE_PACKAGES luci-i18n-package-manager-zh-cn"
 BASE_PACKAGES="$BASE_PACKAGES luci-compat"
 BASE_PACKAGES="$BASE_PACKAGES luci-i18n-firewall-zh-cn"
 BASE_PACKAGES="$BASE_PACKAGES luci-i18n-base-zh-cn"
-# 如果勾选了docker 务必保证软件包大小ROOTFS_PARTSIZE至少大于512 目前1024
-# BASE_PACKAGES="$BASE_PACKAGES luci-i18n-dockerman-zh-cn"
-# 文件管理器
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-dockerman-zh-cn"
 BASE_PACKAGES="$BASE_PACKAGES luci-i18n-filemanager-zh-cn"
-# openlist用来平替alist 但阿里镜像仓库暂时没有 若勾选此项需在repositories.conf中启用官方源
-# 其实可以弯道实现安装openlist 因为istore商店里有该插件 你可以勾选下列的luci-app-store
-#BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openlist-zh-cn"
-
-#打印机相关
 BASE_PACKAGES="$BASE_PACKAGES luci-i18n-p910nd-zh-cn"
-#BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openvpn-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openvpn-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-3cat-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-openvpn-server-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-cloudflared-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-ddns-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-diskman-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-homeproxy-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-msd_lite-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-samba4-zh-cn"
+BASE_PACKAGES="$BASE_PACKAGES luci-i18n-smartdns-zh-cn"
+
+
 
 
 
@@ -36,22 +34,28 @@ BASE_PACKAGES="$BASE_PACKAGES luci-i18n-p910nd-zh-cn"
 # 使用条件:在extra-packages下放置了相关run或者ipk
 CUSTOM_PACKAGES=""
 # 第三方插件 文件传输 luci-app-filetransfer
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lib-fs"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lua-runtime"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-filetransfer"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-filetransfer-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lib-fs"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-lua-runtime"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-filetransfer"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-filetransfer-zh-cn"
 # 第三方插件 argon主题 luci-theme-argon 紫色主题 3个ipk
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-argon-config-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-argon-config"
 
-# 第三方插件 istore 应用商店
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-store"
-# 第三方插件 首页和网络向导
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-quickstart"
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-airconnect"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-autoupdate"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-eqosplus"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-eqosplus"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-oaf"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-onliner"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-subconverter"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-wechatpush"
 
-# 第三方插件 luci-app-adguardhome 去广告
+
+
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
 # 第三方插件 openclash 内核放在files/etc/openclash/core/clash_meta 若不勾选则不集成
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
@@ -65,8 +69,8 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
 # 第三方插件 luci-app-ssr-plus 尤其注意要包含 shadowsocks-libev-ss-server
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-ssr-plus"
 # 第三方插件 luci-app-homeproxy
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-homeproxy-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-homeproxy"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-homeproxy-zh-cn"
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-homeproxy"
 # 第三方插件 luci-app-nikki
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-nikki-zh-cn"
 
@@ -97,18 +101,18 @@ else
 fi
 
 
-# 若构建luci-app-adguardhome 则添加内核
-if echo "$PACKAGES" | grep -q "luci-app-adguardhome"; then
-    echo "✅ [构建逻辑] 已选择 luci-app-adguardhome，添加 AdGuardHome core"
-    if [ -f extra-packages/temp-unpack/AdGuardHome/AdGuardHome ]; then
-        cp extra-packages/temp-unpack/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome
-    else
-        echo "⚠️ [警告] 缺少 AdGuardHome内核,跳过复制,你应该确保extra-packages目录下有相关run文件"
-    fi
-else
-    echo "⚪️ [构建逻辑] 未选择 luci-app-adguardhome"
-    [ -f files/usr/bin/AdGuardHome ] && rm -f files/usr/bin/AdGuardHome
-fi
+# # 若构建luci-app-adguardhome 则添加内核
+# if echo "$PACKAGES" | grep -q "luci-app-adguardhome"; then
+#     echo "✅ [构建逻辑] 已选择 luci-app-adguardhome，添加 AdGuardHome core"
+#     if [ -f extra-packages/temp-unpack/AdGuardHome/AdGuardHome ]; then
+#         cp extra-packages/temp-unpack/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome
+#     else
+#         echo "⚠️ [警告] 缺少 AdGuardHome内核,跳过复制,你应该确保extra-packages目录下有相关run文件"
+#     fi
+# else
+#     echo "⚪️ [构建逻辑] 未选择 luci-app-adguardhome"
+#     [ -f files/usr/bin/AdGuardHome ] && rm -f files/usr/bin/AdGuardHome
+# fi
 
 
 # 开始构建 软件包大小1024代表1GB 
